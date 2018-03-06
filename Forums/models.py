50e06df003b2fd60224efc0d16eff14b7acddd00
Mos'ad Desouky <mosad.desouky@gmail.com>
2018-03-06 23:27:17 +0200

    Creatin the Forum step 1

---
+++ Forums/models.py
@@ -0,0 +1,9 @@
+class Member:
+	def __init__(self,name,age):
+		self.name = name
+		self.age = age
+
+class Post:
+	def __init__(self,Post_Title,Post_Content):
+		self.Post_Title = Post_Title
+		self.Post_Content = Post_Content
