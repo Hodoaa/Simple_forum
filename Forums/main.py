8c6dda2822c79ac6fb7aa5fcef38c01ce5c055e2
mosad desouky shalaby <hodoaa29@gmail.com>
2018-03-11 17:42:42 +0200

    Using Inheritance

--- Forums/main.py
+++ Forums/main.py
@@ -1,156 +1,108 @@
-import models
-import stores
 
-
+import models, stores
+from time import sleep
 
 def create_members():
-    member1 = models.Member("Mohammed", 20)
-    member2 = models.Member("Mohammed", 22)
-    member3 = models.Member("Abdo", 25)
-    print(member1)
-    print(member2)
-    print(member3)
-    print("=" * 30)
+    member1 = models.Member("Ali", 45)
+    member2 = models.Member("Mo'men", 13)
+    member3 = models.Member("mazen", 19)
+    member4 = models.Member("mona", 33)
 
-    return member1, member2, member3
+    return member1, member2, member3, member4
 
 
-def store_should_add_members(members_instances, member_store):
-    for member in members_instances:
-        member_store.add(member)
+def store_members(members, store):
+    for member in members:
+        store.add(member)
 
 
-def stores_should_be_similar():
-    member_store1 = stores.MemberStore()
-    member_store2 = stores.MemberStore()
-    if member_store1.get_all() is member_store2.get_all():
-        print("Same stores elements !")
+def print_all_members(store):
+    for member in store.get_all():
+        print (member)
+
+    print ("=" * 9 + " Print All Members " + "=" * 9)
 
 
-def print_members_list(members_list):
-    for member in members_list:
-        print(member)
+def create_posts():
+    post1 = models.Post("Post1", "this is the post no: 1.", 1)
+    sleep(0.1)
+    post2 = models.Post("Post2", "this is the post no: 2.", 3)
+    sleep(0.1)
+    post3 = models.Post("Post3", "this is the post no: 3.", 2)
+    sleep(0.1)
+    post4 = models.Post("Post4", "this is the post no: 4.", 4)
+    sleep(0.1)
+    post5 = models.Post("Post5", "this is the post no: 5.", 1)
+    sleep(0.1)
+    post6 = models.Post("Post6", "this is the post no: 6.", 3)
+    sleep(0.1)
+    post7 = models.Post("Post7", "this is the post no: 7.", 1)
+    sleep(0.1)
+    post8 = models.Post("Post8", "this is the post no: 8.", 3)
+    sleep(0.1)
+    post9 = models.Post("Post9", "this is the post no: 9.", 1)
+    sleep(0.1)
+    post10 = models.Post("Post10", "this is the post no: 10.", 2)
+
+    return post1, post2, post3, post4, post5, post6, post7, post8, post9, Post10
 
 
-def print_all_members(member_store):
-    print("=" * 30)
-
-    print_members_list(member_store.get_all())
-
-    print("=" * 30)
-
-
-def get_by_id_should_retrieve_same_object(member_store, member2):
-    member2_retrieved = member_store.get_by_id(member2.id)
-
-    if member2 is member2_retrieved:
-        print("member2 and member2_retrieved are matching !")
+def update_member(member, store):
+    copy = models.Member(member.name, member.age)
+    copy.id = member.id
+    print (member)
+    print (copy)
+    copy.name = "Manar"
+    store.update(copy)
+    print (store.get_by_id(member.id))
+    print ("=" * 11 + " Update Member " + "=" * 11)
 
 
-def update_should_modify_object(member_store, member3):
-    member3_copy = models.Member(member3.name, member3.age)
-    member3_copy.id = 3
-
-    if member3_copy is not member3:
-        print("member3 and member3_copy are not the same !")
-
-    print(member3_copy)
-    member3_copy.name = "John"
-    member_store.update(member3_copy)
-    print(member_store.get_by_id(member3.id))
+def search_members_by_name(name, store):
+    results = store.get_by_name("Manar")
+    for member in results:
+        print (member)
+    print ("=" * 10 + " Search Members " + "=" * 10)
 
 
-def store_should_get_members_by_name(member_store):
-
-    print("*" * 30)
-    print("Getting by name:")
-    members_by_name_retrieved = member_store.get_by_name("Mohammed")
-    print_members_list(members_by_name_retrieved)
-
-
-def catch_exception_when_deleting():
-    try:
-        member_store.delete(5)
-    except ValueError:
-        print("It should be an existence entity before deleting !")
+def view_members_with_posts(store, posts):
+    members_with_posts = store.get_members_with_posts(posts)
+    for member in members_with_posts:
+        print ("{0} has posts".format(member.name))
+        for post in member.posts:
+            print ("\t{0}".format(post.title))
+    print("=" * 20)
 
 
-def create_posts(members_instances):
-
-    post1 = models.Post("Agriculture", "Agriculture is amazing", members_instances[0].id)
-    post2 = models.Post("Engineering", "I love engineering", members_instances[0].id)
-
-    post3 = models.Post("Medicine", "Medicine is great", members_instances[1].id)
-    post4 = models.Post("Architecture", "Spectacular art", members_instances[1].id)
-    post5 = models.Post("Astronomy", "Space is awesome", members_instances[1].id)
-
-    post6 = models.Post("Geology", "Earth is our friend", members_instances[2].id)
-    post7 = models.Post("ComputerSci", "Our passion", members_instances[2].id)
-    post8 = models.Post("Algorithms", "Yeah, more of that", members_instances[2].id)
-    post9 = models.Post("Operating Systems", "Ewww", members_instances[2].id)
-
-    print(post1)
-    print(post2)
-    print(post3)
-    print("=" * 30)
-
-    return post1, post2, post3, post4, post5, post6, post7, post8, post9
+def view_top_members_with_posts(store):
+    top_members_with_posts = store.get_top_members_with_posts()
+    for member in top_members_with_posts:
+        print ("{0} has {1} posts".format(member.name, len(member.posts)))
+    print("=" * 20)
 
 
-def store_should_add_posts(posts_instances, post_store):
-    for member in posts_instances:
-        post_store.add(member)
+def store_posts(posts, store):
+    for post in posts:
+        store.add(post)
 
 
-def store_should_get_members_with_posts(member_store, post_store):
-    members_with_posts = member_store.get_members_with_posts(post_store.get_all())
-
-    for member_with_posts in members_with_posts:
-        print f("{member_with_posts} has posts:")
-        for post in member_with_posts.posts:
-            print f("\t{post}")
-
-        print("=" * 10)
+def view_posts_by_date(store):
+    posts_by_date = store.get_posts_by_date()
+    for post in posts_by_date:
+        print ("{0} created at {1}".format(post.title, post.date))
+    print("=" * 20)
 
 
-def store_should_get_top_two(member_store, post_store):
-    top_two_members = member_store.get_top_two(post_store.get_all())
-
-    for member_with_posts in top_two_members:
-        print f("{member_with_posts} has posts:")
-        for post in member_with_posts.posts:
-            print f("\t{post}")
-
-members_instances = create_members()
-member1, member2, member3 = members_instances
-
+members = create_members()
 member_store = stores.MemberStore()
+store_members(members, member_store)
+# print_all_members(member_store)
+# update_member(members[3], member_store)
+# search_members_by_name("Manar", member_store)
 
-store_should_add_members(members_instances, member_store)
-
-stores_should_be_similar()
-
-print_all_members(member_store)
-
-get_by_id_should_retrieve_same_object(member_store, member2)
-
-update_should_modify_object(member_store, member3)
-
-catch_exception_when_deleting()
-
-print_all_members(member_store)
-
-store_should_get_members_by_name(member_store)
-
-
-
-posts_instances = create_posts(members_instances)
-post1, post2, post3, post4, post5, post6, post7, post8, post9 = posts_instances
-
+posts = create_posts()
 post_store = stores.PostStore()
+store_posts(posts, post_store)
 
-store_should_add_posts(posts_instances, post_store)
-
-store_should_get_members_with_posts(member_store, post_store)
-
+# view_mem
 store_should_get_top_two(member_store, post_store)
--- Forums/models.py
+++ Forums/models.py
@@ -5,12 +5,15 @@ class Member:
 		self.age = age
 		self.posts = []
 	def __str__(self):
-		return f"Name: {self.name}, Age: {self.age}"
+		return "{0}, {1} Years Old".format(self.name, self.age)
+#-------------------------------------------------------------------------
 class Post:
 	def __init__(self,Post_Title,Post_Content,member_id = 0):
 		self.id = 0
+		self.date = datetime.datetime.now()
 		self.Post_Title = Post_Title
 		self.Post_Content = Post_Content
 		self.member_id = member_id
 	def __str__(self):
-		return f("Post_Title:  {self.Post_Title}, Post_Content: {self.Post_Content}")
+		return "{0}: {1}...".format(self.Post_Title, self.Post_Content[:10])
+#----------------------------------------------------------------------------
